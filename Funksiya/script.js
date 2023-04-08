de6776493ea23a8cea662220e0c2b8411bf5efb2
suhrob <suhrobrahmatulayev973132@gmail.com>
2023-04-08 14:25:13 +0500

    3

--- Funksiya/script.js
+++ Funksiya/script.js
@@ -15,3 +15,36 @@
 //     }
 // }
 // sikl(6,22,3)
+
+
+
+//                                                uyga vazifa
+
+//   1
+
+// let array = ['Murod' , 'Malik' , 'Lola' , 'Shaxlo', 'Fariz' , 'Bobur' , 'Shoxruh' , 'Suhrob']
+
+// function add(a){
+//     massive1 = array.slice(0,array / 2)
+//     massive2 = array.slice(massive1, array.length)
+//     array.push(massive1 + 'salim' + massive2)
+//     return array
+// }   
+
+// add(2)
+// console.log(add());
+
+
+//   3
+
+// let array = [11 , 'hl' , 'ghfhgfhfd' , 3255 , 'q32432']
+// let num = []
+// let str = []
+// array.forEach(element => {
+//     if(typeof element === "string"){
+//         str.push(element)
+//     }else if(typeof element === 'number'){
+//         num.push(element)
+//     }
+// });
+// console.log(num , str);
